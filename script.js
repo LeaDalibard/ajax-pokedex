6c7ffb88aa4d3a20c7e0c2a0ddacd61786de9df0
Lea Dalibard <leadalibard@gmail.com>
2020-09-14 11:20:35 +0200

    Getting Pokemon name and Id with fetching from the input typed  and inner-html them

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 (() => {
-    fetch("https://pokeapi.co/api/v2/")
-        .then(response=>response.json())
-        .then(pokemon=>{
-            console.log(pokemon)})
+
+    search.addEventListener("click", getPokemon)
+
+    function getPokemon() {
+        let pokeName = document.getElementById("pokemon").value
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokeName + "/")
+            .then(response => response.json())
+            .then(pokemon => {
+                    document.getElementById("idNum").innerHTML = pokemon.id
+                    document.getElementById("name").innerHTML = pokemon.name
+                }
+            )
+
+
+    }
 })();
+/*
+ pokemons => {
+                    pokemons.forEach(
+                        (pokemon) => {
+                            if (pokemon.name = pokeName) {
+                                console.log(pokemon)
+                            }
+                        }
+                    )
+                }
+ */
