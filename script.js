6a0e3b34b15d16d757708063ea9deb43abac8e80
Lea Dalibard <leadalibard@gmail.com>
2020-09-14 12:29:22 +0200

    Saving before lunch, array move turned to a random 4 array,  trying to keep just names

--- script.js
+++ script.js
@@ -9,10 +9,24 @@
             .then(pokemon => {
                     document.getElementById("idNum").innerHTML = pokemon.id
                     document.getElementById("name").innerHTML = pokemon.name
+                    let arrMoves = pokemon.moves
+                    let moves = []
+                    const randomArray = []
+                    for (let i = 0; i < 4; i++) {
+                        randomArray.push(Math.floor(Math.random() * arrMoves.length))
+                    }
+                    if (arrMoves.length <= 4) {
+                        moves = arrMoves.name
+                    } else {
+                        for (let i = 0; i < 4; i++) {
+                            moves.push(arrMoves[randomArray[i]])
+                        }
+                    }
+                    document.getElementById("pokMove").innerHTML = moves
+                    console.log(moves)
+
                 }
             )
-
-
     }
 })();
 /*
