2406aa5680fa66ce080fe41a49f9a42b2da937fd
Lea Dalibard <leadalibard@gmail.com>
2020-09-15 12:04:41 +0200

    Saving previous evolution working, going to design

--- script.js
+++ script.js
@@ -31,19 +31,18 @@
         fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokeName + "/")
             .then(response => response.json())
             .then(pokemon => {
+                var nextEvol=pokemon
                 if(pokemon.evolves_from_species==null){ document.getElementById("preEvolname").innerHTML = "First evolution"}
                 else {
                     prevEvol = pokemon.evolves_from_species.name
-                    document.getElementById("preEvolname").innerHTML = prevEvol
+                    document.getElementById("preEvolname").innerHTML += prevEvol
                     urlPrev = "https://pokeapi.co/api/v2/pokemon/" + prevEvol + "/"
                     return fetch(urlPrev)
                         .then(response => response.json())
                         .then(evolPrev => {
                             imgPrev = evolPrev.sprites
-                            console.log(evolPrev)
                             document.getElementById("preEvol-1").src = imgPrev.front_default
                         })
-
                 }
 
             })
@@ -52,39 +51,3 @@
 
     }
 })();
-/*
-
- function groupBy(OurArray, property) {
-        return OurArray.reduce(function (accumulator, object) {
-            // get the value of our object(age in our case) to use for group    the array as the array key
-            const key = object[property];
-            // if the current value is similar to the key(age) don't accumulate the transformed array and leave it empty
-            if (!accumulator[key]) {
-                accumulator[key] = [];
-            }
-            // add the value to the array
-            accumulator[key].push(object);
-            // return the transformed array
-            return accumulator;
-// Also we also set the initial value of reduce() to an empty object
-        }, {});
-    }
-
-let arrMoves = pokemon.moves
-                    let moves = []
-                    const randomArray = []
-                    var keys = Object.values(arrMoves);
-                    for (let i = 0; i < 4; i++) {
-                         randomArray.push(Math.floor(Math.random() * arrMoves.length))
-                     }
-                     if (arrMoves.length <= 4) {
-                         moves = arrMoves.name
-                     } else {
-                         for (let i = 0; i < 4; i++) {
-                             moves.push(arrMoves[randomArray[i]])
-                         }
-                     }
-                    document.getElementById("pokMove").innerHTML = Object.values(moves)
-                    console.log(Object.values(moves))
-                    console.log(keys)
- */
